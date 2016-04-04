36fc0e88447bff60e19300d6084a20db8006014f
Vincent Istas <vistas@healey.be>
2016-04-04 21:56:36 +0100

    Update f5_GTM_DNS-FILTER.tcl

--- f5_GTM_DNS-FILTER.tcl
+++ f5_GTM_DNS-FILTER.tcl
@@ -1,7 +1,7 @@
 when DNS_REQUEST {
   set req [string tolower [DNS::question name]]
   if {$req  contains ".domain.ext" }{
-     log local0. "SRB query: $req"
+     log local0. "ALLOW query: $req"
    }
   else {
      log local0. "DROP query: $req"
